6926b9a2bc30fbf39399d84e39f4fd412da0675a
Nikita Negi <nikitanegi044@gmail.com>
2023-11-17 20:03:48 +0530

    first

---
+++ src/components/helloWorld.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class myComp extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <h1>
+                    Hello World
+                </h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default myComp
