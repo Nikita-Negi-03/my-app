e6ef1b6bdc2369b3dd03d0c703ccd62421255a80
Nikita Negi <nikitanegi044@gmail.com>
2023-11-18 13:16:13 +0530

    apis

--- src/serverfiles/routes.js
+++ src/serverfiles/routes.js
@@ -1,5 +1,8 @@
 var functions = require("./students/student")
 module.exports = function (app) {
+    app.post("/registerUser",functions.register)
+    app.post("/userLogin",functions.userLogin)
     app.get("/allStudents", functions.allStudents);
-    app.post("/enrollStudent", functions.enrollStudent)
+    app.post("/enrollStudent", functions.enrollStudent);
+    app.get("/getStudent/:studentId",functions.studentDetails)
 }
